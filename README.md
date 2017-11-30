99c1ad832c6053ecde0039878bc24fa70fed5b9f
Marcos Rachid <marcos.rachid@matera.com>
2017-11-30 13:08:53 -0200

    modules and components

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # angular-layout-structure
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.5.
---
+++ src/app/layout/page1/page1.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Page1Component } from './page1.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [Page1Component]
+})
+export class Page1Module { }
