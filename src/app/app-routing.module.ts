9bf54895a1bd60328e2dcb37e5e7de358edc1ee5
Marcos Rachid <marcosrachid@gmail.com>
2017-12-11 23:31:20 -0200

    Correcting Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppGuard } from './';
-
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [ AppGuard ]},
+  { path: '', loadChildren: './layout/layout.module#LayoutModule'},
   { path: '404', loadChildren: './not-found/not-found.module#NotFoundModule'},
   { path: 'sign-in', loadChildren: './sign-in/sign-in.module#SignInModule'},
   { path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpModule'},
--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppGuard } from '../_shared';
+
 import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [ AppGuard ],
+    // canActivateChild: [ AppGuard ],
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', loadChildren: './page1/page1.module#Page1Module' },
